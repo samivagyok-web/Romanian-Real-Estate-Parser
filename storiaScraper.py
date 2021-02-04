de7071161383b8bc05f1f10a5f731a3ba6865889
samivagyok-web <66529115+samivagyok-web@users.noreply.github.com>
2021-02-04 14:30:28 +0200

    Update storiaScraper.py

--- storiaScraper.py
+++ storiaScraper.py
@@ -1,33 +1,55 @@
 import re
+import csv
 import requests
 from bs4 import BeautifulSoup
 
-URL = 'https://www.storia.ro/vanzare/apartament/bihor/oradea/'
-page = requests.get(URL)
 
+REGION = "bihor"
+TOWN = "oradea"
+OFFEREDFOR = "vanzare" # vanzare/inchiriere
+URL = f'https://www.storia.ro/{OFFEREDFOR}/apartament/{REGION}/{TOWN}/?page=1'
+
+page = requests.get(URL)
 soup = BeautifulSoup(page.content, 'lxml')
 
-listing2 = soup.find('div', class_="listing")
-offerDetails = listing2.find_all('div', class_="offer-item-details")
-for offer in offerDetails:
-    params = offer.find('ul', class_="params")
+numOfPages = soup.find('ul', class_="pager")
+numOfPages = numOfPages.find_all('li', class_="")
+maxPage = 0
+for found in numOfPages:
+    maxPage = int(found.a.text)
 
-    rooms = offer.find('li', class_="offer-item-rooms hidden-xs")   
 
-    sqMeter = offer.find('li', class_="hidden-xs offer-item-area")
+with open('estates.csv', 'w', newline='') as file:
+    csv_writer = csv.writer(file)
+    csv_writer.writerow(['Number of rooms', 'Area (m2)', 'Price (€)', 'Price per m2 (€)', 'Zone', 'Link'])
 
-    price = offer.find('li', class_="offer-item-price")
-    price = price.text.strip()    
+    for page in range (1, maxPage):
+        URL = f'https://www.storia.ro/{OFFEREDFOR}/apartament/{REGION}/{TOWN}/?page={page}'
 
-    pricePerSq = offer.find('li', class_="hidden-xs offer-item-price-per-m")
+        page = requests.get(URL)
+        soup = BeautifulSoup(page.content, 'lxml')
 
-    zone = offer.find('p', class_="text-nowrap")
-    zone = zone.text
-    pattern = re.compile(r'(?<=\),\s){1}\w+([-\s{1}$](\w+|\s{1})|$)')
-    matches = pattern.finditer(zone)
-    for match in matches:
-        zone = match.group(0)
+        listing2 = soup.find('div', class_="listing")
+        offerDetails = listing2.find_all('div', class_="offer-item-details")
+        for offer in offerDetails:
+            params = offer.find('ul', class_="params")
 
-    linkk = offer.find('a')['href']
+            rooms = offer.find('li', class_="offer-item-rooms hidden-xs")   
 
-    print(f"{rooms.text} -- {sqMeter.text} -- {price} -- {pricePerSq.text} -- {zone} -- {linkk}")
+            sqMeter = offer.find('li', class_="hidden-xs offer-item-area")
+
+            price = offer.find('li', class_="offer-item-price")
+            price = price.text.strip()    
+
+            pricePerSq = offer.find('li', class_="hidden-xs offer-item-price-per-m")
+
+            zone = offer.find('p', class_="text-nowrap")
+            zone = zone.text
+            pattern = re.compile(r'(?<=\),\s){1}\w+([-\s{1}$](\w+|\s{1})|$)')
+            matches = pattern.finditer(zone)
+            for match in matches:
+                zone = match.group(0)
+
+            linkk = offer.find('a')['href']
+
+            csv_writer.writerow([rooms.text, sqMeter.text, price, pricePerSq.text, zone, linkk])
