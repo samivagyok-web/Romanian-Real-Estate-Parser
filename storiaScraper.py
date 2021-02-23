aa6800c926805c4f9c25291b6839563b0a100319
samivagyok-web <66529115+samivagyok-web@users.noreply.github.com>
2021-02-23 11:07:04 +0200

    Added postings for sale

--- storiaScraper.py
+++ storiaScraper.py
@@ -1,7 +1,20 @@
 import re
 import csv
+from typing import Pattern, Text
 import requests
+import time
 from bs4 import BeautifulSoup
+
+def main():
+    start = time.time()
+    REGION = "bihor"
+    TOWN = "oradea"
+
+    forSale(REGION, TOWN)
+    forRent(REGION, TOWN)
+    finish = time.time()
+
+    print(finish - start)
 
 def dataToInt(data):
     sq = 0
@@ -9,73 +22,117 @@ def dataToInt(data):
     for match in pattern.finditer(data):
         sq = match.group(0)
     return int(sq)
-
+ 
 def priceToInt(cost):
     money = ""
     for digit in cost:
-        if digit >= '0' and digit <= '9':
-            money = money + digit
-    return int(money)
-
-def main():
-    REGION = "bihor"
-    TOWN = "oradea"
-    OFFEREDFOR = "vanzare" # vanzare/inchiriere
-    URL = f'https://www.storia.ro/{OFFEREDFOR}/apartament/{REGION}/{TOWN}/?page=1'
-
-    page = requests.get(URL)
-    soup = BeautifulSoup(page.content, 'lxml')
-
+        if digit >= '0' and digit <= '9' or digit == ',':
+            if (digit == ','):
+                money = money + '.'
+            else:
+                money = money + digit
+    return float(money)
+def maxPageNum(URL, page, soup):
     numOfPages = soup.find('ul', class_="pager")
     numOfPages = numOfPages.find_all('li', class_="")
     maxPage = 0
     for found in numOfPages:
         maxPage = int(found.a.text)
+    return maxPage
 
+def forSale(REGION, TOWN):
+    URL = f'https://www.storia.ro/vanzare/apartament/{REGION}/{TOWN}/?nrAdsPerPage=72&page=1'
 
-    with open('estates.csv', 'w', newline='') as file:
+    page = requests.get(URL)
+    soup = BeautifulSoup(page.content, 'lxml')
+ 
+    maxPage = maxPageNum(URL, page, soup)
+ 
+    with open('estatesForSale.csv', 'w', newline='') as file:
         csv_writer = csv.writer(file)
         csv_writer.writerow(['Number of rooms', 'Area (m2)', 'Price (€)', 'Price per m2 (€)', 'Zone', 'Link'])
-
+ 
         for page in range (1, maxPage):
-            URL = f'https://www.storia.ro/{OFFEREDFOR}/apartament/{REGION}/{TOWN}/?page={page}'
-
+            URL = f'https://www.storia.ro/vanzare/apartament/{REGION}/{TOWN}/?nrAdsPerPage=72&page={page}'
+ 
             page = requests.get(URL)
             soup = BeautifulSoup(page.content, 'lxml')
-
+ 
             listing2 = soup.find('div', class_="listing")
             offerDetails = listing2.find_all('div', class_="offer-item-details")
             for offer in offerDetails:
                 params = offer.find('ul', class_="params")
-
+ 
                 rooms = offer.find('li', class_="offer-item-rooms hidden-xs")   
-
+ 
                 roomNum = dataToInt(rooms.text)
                 sqMeter = offer.find('li', class_="hidden-xs offer-item-area")
-
-                sqMeterNum = dataToInt(sqMeter.text)
+ 
+                sqMeterNum = priceToInt(sqMeter.text)
 
                 price = offer.find('li', class_="offer-item-price")
-
+ 
                 price = price.text.strip()    
-
+ 
                 intPrice = priceToInt(price)
-
+ 
                 pricePerSq = offer.find('li', class_="hidden-xs offer-item-price-per-m")
-
+ 
                 intPriceSquare = priceToInt(pricePerSq.text)
-
+ 
                 zone = offer.find('p', class_="text-nowrap")
                 zone = zone.text
-                pattern = re.compile(r'(?<=\),\s){1}\w+([-\s{1}$](\w+|\s{1})|$)')
-                matches = pattern.finditer(zone)
-
-                for match in matches:
-                    zone = match.group(0)
-
+                zone: Text = zone.split(',')[-1].strip()               
+                    
                 linkk = offer.find('a')['href']
-
-                csv_writer.writerow([roomNum, sqMeterNum, intPrice, intPriceSquare, zone, linkk])
  
+                csv_writer.writerow([roomNum, sqMeterNum, intPrice, intPriceSquare, zone, linkk])
+    return
+
+def forRent(REGION, TOWN):
+    URL = f'https://www.storia.ro/inchiriere/apartament/{REGION}/{TOWN}/?nrAdsPerPage=72&page=1'
+
+    page = requests.get(URL)
+    soup = BeautifulSoup(page.content, 'lxml')
+ 
+    maxPage = maxPageNum(URL, page, soup)
+ 
+    with open('estatesForRent.csv', 'w', newline='') as file:
+        csv_writer = csv.writer(file)
+        csv_writer.writerow(['Number of rooms', 'Area (m2)', 'Price/month (€)', 'Zone', 'Link'])
+ 
+        for page in range (1, maxPage):
+            URL = f'https://www.storia.ro/inchiriere/apartament/{REGION}/{TOWN}/?nrAdsPerPage=72&page={page}'
+ 
+            page = requests.get(URL)
+            soup = BeautifulSoup(page.content, 'lxml')
+ 
+            listing2 = soup.find('div', class_="listing")
+            offerDetails = listing2.find_all('div', class_="offer-item-details")
+            for offer in offerDetails:
+                params = offer.find('ul', class_="params")
+ 
+                rooms = offer.find('li', class_="offer-item-rooms hidden-xs")   
+ 
+                roomNum = dataToInt(rooms.text)
+                sqMeter = offer.find('li', class_="hidden-xs offer-item-area")
+ 
+                sqMeterNum = priceToInt(sqMeter.text)
+
+                price = offer.find('li', class_="offer-item-price")
+ 
+                price = price.text.strip()    
+ 
+                intPrice = priceToInt(price)
+ 
+                zone = offer.find('p', class_="text-nowrap")
+                zone = zone.text
+                zone = zone.split(',')[-1].strip()
+ 
+                linkk = offer.find('a')['href']
+ 
+                csv_writer.writerow([roomNum, sqMeterNum, intPrice, zone, linkk])
+    return    
+
 if __name__ == "__main__":
     main()
